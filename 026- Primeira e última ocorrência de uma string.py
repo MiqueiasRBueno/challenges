1c31d3b00793f0175e1417ab61fb69dbedc86bbc
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-03-10 17:21:56 -0300

    Create 026- Primeira e última ocorrência da string.py

--- 026- Primeira e última ocorrência de uma string.py
+++ 026- Primeira e última ocorrência de uma string.py
@@ -8,3 +8,4 @@ letter = str(input('Digite uma letra para buscar na frase: ')).upper()
 print(f'A letra "{letter}" aparece {first_last_occurrence.count(letter)} vezes na frase acima!')
 print(f'A letra "{letter}" aparece primeiro na posição {first_last_occurrence.find(letter) + 1} da frase.')
 print(f'E a última posição que aparece é {first_last_occurrence.rfind(letter) + 1}')
+
