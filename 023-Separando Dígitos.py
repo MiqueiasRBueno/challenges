eb3f8513af6825304547ec4006ac1d6d2d4553a0
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-03-10 16:51:09 -0300

    Create 023- Separando Dígitos

---
+++ 023-Separando Dígitos.py
@@ -0,0 +1,12 @@
+# Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um de seus dígitos separados:
+# unidade
+# dezena
+# centena
+# milhar
+
+separating_digits = int(input('Digite um número ente 0 e 9999: '))
+unit = separating_digits // 1 % 10
+ten = separating_digits // 10 % 10
+hundreds = separating_digits // 100 % 10
+thousand = separating_digits // 1000 % 10
+print(f'O número dígitado tem:\n{unit} unidade\n{ten} dezenas\n{hundreds} dezenas\n{thousand} milhar')
