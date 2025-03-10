9fdb269a676a7ab3e0c9f74b2ab91672d28ded6c
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-03-10 17:21:05 -0300

    Create 023- Separando Dígitos

--- 026- Primeira e última ocorrência de uma string.py
+++ 026- Primeira e última ocorrência de uma string.py
@@ -3,3 +3,8 @@
 # Em que posição aparece a primeira vez.
 # Em que posição apare a última vez.
 
+first_last_occurrence = str(input('Digite uma frase qualquer: ')).strip().upper()
+letter = str(input('Digite uma letra para buscar na frase: ')).upper()
+print(f'A letra "{letter}" aparece {first_last_occurrence.count(letter)} vezes na frase acima!')
+print(f'A letra "{letter}" aparece primeiro na posição {first_last_occurrence.find(letter) + 1} da frase.')
+print(f'E a última posição que aparece é {first_last_occurrence.rfind(letter) + 1}')
