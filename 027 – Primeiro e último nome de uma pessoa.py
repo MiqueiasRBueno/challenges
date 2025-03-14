6312d273540397e69f3e17f6a613d2b2f4544530
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-03-14 11:14:12 -0300

    Create 025 - Procurando uma string dentro de outra.py

--- 027 – Primeiro e último nome de uma pessoa.py
+++ 027 – Primeiro e último nome de uma pessoa.py
@@ -4,4 +4,5 @@
 cores = {'azul':'\033[1;34m', 'limpa': '\033[m', 'vermelho': '\033[1;31m', 'amarelo': '\033[33m'}
 name = str(input('Digite seu nome completo: ')).strip().split()
 print(f'Seu primeiro nome é {cores['amarelo']}{name[0].title()}{cores['limpa']}')
-print(f'Seu último nome é {cores['vermelho']}{name[-1].title()}{cores['limpa']}')
+if len(name) > 1:
+    print(f'Seu último nome é {cores['vermelho']}{name[-1].title()}{cores['limpa']}')
