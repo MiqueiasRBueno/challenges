8c724a3ff65ebf88b2f915c7048aada558decae1
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-03-14 14:32:36 -0300

    update 028- Radar eletrônico.py

--- 029 – Radar eletrônico.py
+++ 029 – Radar eletrônico.py
@@ -6,18 +6,21 @@ cores = {'branco': '\033[30m', 'vermelho': '\033[1;31m', 'verde': '\033[32m', 'a
          'azul':'\033[1;34m', 'roxo': '\033[35m', 'ciano': '\033[7:36m', 'cinza': '\033[37m', 'limpa': '\033[m'}
 
 # interação com o usuário
-speed = int(input('Digite a velocidade atual do veículo: '))
-# compara se a informação é verdadeira ou falsa
-if speed > 80:
-    # calcula o valor da multa a ser aplicado
-    fine = (speed - 80) * 7
-    # mensagem para o motorista multado, desrespeitou os limites de velocidade
-    print(f'''    Você foi multado, excedeu o limite de velocidade!
-    Velocidade máxima da via {cores['vermelho']}80{cores['limpa']} km
-    Valor da sua multa aplicada R$ {cores['vermelho']}{fine:.2f}{cores['limpa']}
-    {cores['amarelo']}Dirija com cuidado!{cores['limpa']}''')
-else:
-    # mensagem para o motorista que respeitou os limites de velocidade
-    print(f'''    Meus parabéns, você respeitou os limites de velocidade!
-    Velocidade máxima da via {cores['vermelho']}80{cores['limpa']} km
-    {cores['amarelo']}Continue assim, respeitando a vida!{cores['limpa']}''')
+try:
+    speed = int(input('Digite a velocidade atual do veículo: '))
+    # compara se a informação é verdadeira ou falsa
+    if speed > 80:
+        # calcula o valor da multa a ser aplicado
+        fine = (speed - 80) * 7
+        # mensagem para o motorista multado, desrespeitou os limites de velocidade
+        print(f'''    Você foi multado, excedeu o limite de velocidade!
+        Velocidade máxima da via {cores['vermelho']}80{cores['limpa']} km
+        Valor da sua multa aplicada R$ {cores['vermelho']}{fine:.2f}{cores['limpa']}
+        {cores['amarelo']}Dirija com cuidado!{cores['limpa']}''')
+    else:
+        # mensagem para o motorista que respeitou os limites de velocidade
+        print(f'''    Meus parabéns, você respeitou os limites de velocidade!
+        Velocidade máxima da via {cores['vermelho']}80{cores['limpa']} km
+        {cores['amarelo']}Continue assim, respeitando a vida!{cores['limpa']}''')
+except ValueError:
+    print(f'{cores['vermelho']}Erro: Por favor, insirá um número válido: {cores['limpa']}')
