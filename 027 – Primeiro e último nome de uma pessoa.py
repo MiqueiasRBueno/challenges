ba6ca624c43e9209ce6b1b49bd10fc101c7294b4
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-03-14 11:18:40 -0300

    Create 025 - Procurando uma string dentro de outra.py

--- 027 – Primeiro e último nome de uma pessoa.py
+++ 027 – Primeiro e último nome de uma pessoa.py
@@ -1,8 +1,13 @@
 # Faça um programa que leia o nome completo de uma pessoa,
 # mostrando em seguida o primeiro e o último nome separadamente.
 
+# dicionário de cores
 cores = {'azul':'\033[1;34m', 'limpa': '\033[m', 'vermelho': '\033[1;31m', 'amarelo': '\033[33m'}
+# variável de inserção de dados
 name = str(input('Digite seu nome completo: ')).strip().split()
+# mostra na tela o primeiro nome
 print(f'Seu primeiro nome é {cores['amarelo']}{name[0].title()}{cores['limpa']}')
+# Caso tenha mais de um some:
 if len(name) > 1:
+    # mostra o último nome na tela
     print(f'Seu último nome é {cores['vermelho']}{name[-1].title()}{cores['limpa']}')
