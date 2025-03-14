ff60d36beb8a6b0911500e9da077534e0babe7f6
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-03-14 14:17:13 -0300

    Create 031 – Custo da Viagem.py

---
+++ 031 – Custo da Viagem.py
@@ -0,0 +1,6 @@
+# Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem,
+# cobrando R$ 0,50 por km para viagens de até 200Km e R$ 0,45 parta viagens mais longas.
+
+# dicionário de cores
+cores = {'branco': '\033[30m', 'vermelho': '\033[1;31m', 'verde': '\033[32m', 'amarelo': '\033[33m',
+         'azul':'\033[1;34m', 'roxo': '\033[35m', 'ciano': '\033[7:36m', 'cinza': '\033[37m', 'limpa': '\033[m'}
