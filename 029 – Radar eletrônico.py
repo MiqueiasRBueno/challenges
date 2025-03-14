4a7cb000e251db95eddc5d27c92ccb8598af946e
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-03-14 14:23:05 -0300

    Create 028- Radar eletrônico.py

--- 029 – Radar eletrônico.py
+++ 029 – Radar eletrônico.py
@@ -1,5 +1,5 @@
-# Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h,
-# mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7.00 por cada km acima do limite.
+# Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80 km/h,
+# mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$ 7.00 por cada km acima do limite.
 
 # dicionário de cores
 cores = {'branco': '\033[30m', 'vermelho': '\033[1;31m', 'verde': '\033[32m', 'amarelo': '\033[33m',
@@ -9,10 +9,15 @@ cores = {'branco': '\033[30m', 'vermelho': '\033[1;31m', 'verde': '\033[32m', 'a
 speed = int(input('Digite a velocidade atual do veículo: '))
 # compara se a informação é verdadeira ou falsa
 if speed >= 80.1:
+    # calcula o valor da multa a ser aplicado
+    fine = (speed - 80) * 7
+    # mensagem para o motorista multado, desrespeitou os limites de velocidade
     print(f'''    Você foi multado, excedeu o limite de velocidade!
     Velocidade máxima da via {cores['vermelho']}80{cores['limpa']} km
+    Valor de sua multa R$ {cores['vermelho']}{fine:.2f}{cores['limpa']}
     {cores['amarelo']}Dirija com cuidado!{cores['limpa']}''')
 else:
+    # mensagem para o motorista que respeitou os limites de velocidade
     print(f'''    Meus parabéns, você respeitou os limites de velocidade!
     Velocidade máxima da via {cores['vermelho']}80{cores['limpa']} km
     {cores['amarelo']}Continue assim, respeitando a vida!{cores['limpa']}''')
