e6699bdf61ffc3a62c43ff0838ed287858710519
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-03-17 17:37:56 -0300

    Create 033 – Maior e menor valores

--- 033 – Maior e menor valores.py
+++ 033 – Maior e menor valores.py
@@ -1,10 +1,15 @@
 # Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
 
+# dicionário de cores
+cores = {'branco': '\033[30m', 'vermelho': '\033[1;31m', 'verde': '\033[1;32m', 'amarelo': '\033[33m',
+         'azul':'\033[1;34m', 'roxo': '\033[35m', 'ciano': '\033[7;36m', 'cinza': '\033[37m', 'limpa': '\033[m'}
+
+# Variáveis que receberão os maiores e menores valores
 biggest = smallest = 0
 # Laço de repetição
 for c in range(1, 4):
     # Variável para inserção dos dados
-    number_biggest_smallest = float(input(f'Digite o {c}° número qualquer: '))
+    number_biggest_smallest = int(input(f'Digite o \033[1;3{c}m{c}°\033[m número qualquer: '))
     # Analisa se é o primeiro dado inserido
     if c == 1:
         # as variáveis menor e maior recebem os dados digitados pelo usuário
@@ -17,5 +22,5 @@ for c in range(1, 4):
         # compara se o número digitado é o menor e coloca na variável menor
         if number_biggest_smallest < smallest:
             smallest = number_biggest_smallest
-print(f'''{biggest}
-{smallest}''')
+print(f'''O maior valor digitado foi {cores['azul']}{biggest}{cores['limpa']}
+E o menor valor digitado foi o {cores['verde']}{smallest}{cores['limpa']}''')
