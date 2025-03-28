0c0d216b8dafa0e0b5391dfe4918fc2bf4a1ce66
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-03-28 11:23:50 -0300

    Rename 044 – Gerenciador de Pagamentos

--- 044 – Gerenciador de Pagamentos.py
+++ 044 – Gerenciador de Pagamentos.py
@@ -5,21 +5,44 @@
 # – em até 2x no cartão: preço formal
 # – 3x ou mais no cartão: 20% de juros
 
-print(f'\033[1;32m{'=' * 65}\033[m')
+print(f'''\033[1;32m{'=' * 65}\033[m
+{' ' * 18}\033[1;33mEscolha a forma de pagamento:\033[m
+\033[1;32m{'=' * 65}\033[m''')
 preco_produto = float(input('Digite o valor do produto: '))
 print(f'''\033[1;32m{'=' * 65}\033[m
-{' ' * 18}\033[1mEscolha a forma de pagamento:\033[m
-\033[1;32m{'=' * 65}\033[m
 [1] Para pagamento à vista em dinheiro / Pix: Com 10% de desconto
 [2] Para pagamento à vista no cartão: Com 5% de desconto
 [3] Para pagamento em até 2X no cartão: Preço formal
 [4] Para pagamento em 3X ou mais no cartão: 20% de juros
 ''')
 forma_pagamento = int(input('Escolha uma das formas de pagamento possíveis: '))
-print(f'''\033[1;32m{'=' * 65}\033[m
-A forma escolhida foi à vista [dinheiro/ Pix]''')
+print(f'''\033[1;32m{'=' * 65}\033[m''')
 if forma_pagamento == 1:
-    print(f'''Você terá um desconto de 10% sobre o valor do produto
-Preço Formal R$ {preco_produto:.2f}
-Total a Pagar com desconto R$ {preco_produto - (preco_produto * 0.10)}
+    print(f'''A forma escolhida foi à vista [dinheiro/ Pix]
+Você terá um desconto de 10% sobre o valor do produto
+Preço Formal_________________R$ {preco_produto:.2f}
+Desconto_____________________R$ \033[4m{preco_produto * 0.1:.2f}\033[m
+Total a Pagar com desconto___R$ {preco_produto - (preco_produto * 0.1):.2f}
 \033[1;32m{'=' * 65}\033[m''')
+elif forma_pagamento == 2:
+    print(f'''A forma escolhida foi à vista no cartão
+Você terá um desconto de 5% sobre o valor do produto
+Preço Formal_________________R$ {preco_produto:.2f}
+Desconto_____________________R$ \033[4m{preco_produto * 0.05:.2f}\033[m
+Total a Pagar com desconto___R$ {preco_produto - (preco_produto * 0.05):.2f}
+\033[1;32m{'=' * 65}\033[m''')
+elif forma_pagamento == 3:
+    print(f'''A forma escolhida foi em 2x no cartão
+Você pagará o preço formal
+Preço Formal_________________R$ {preco_produto:.2f}
+Desconto_____________________R$ \033[4m00.00\033[m
+Total a Pagar________________R$ {preco_produto:.2f}
+\033[1;32m{'=' * 65}\033[m''')
+elif forma_pagamento == 4:
+    print(f'''A forma escolhida foi parcelado no cartão
+Você terá um acréscimo de 20% sobre o valor do produto
+Preço Formal_________________R$ {preco_produto:.2f}
+Desconto_____________________R$ \033[4m{preco_produto * 0.20:.2f}\033[m
+Total a Pagar com desconto___R$ {preco_produto + (preco_produto * 0.20):.2f}
+\033[1;32m{'=' * 65}\033[m''')
+print('Obrigado pela preferência, volte sempre!')
