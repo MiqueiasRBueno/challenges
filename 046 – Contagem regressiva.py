99ae72b5c60cdef9cce046da289c6efb2370160b
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-04-02 14:06:11 -0300

    Rename 046 – Contagem regressiva

---
+++ 046 – Contagem regressiva.py
@@ -0,0 +1,13 @@
+# Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício,
+# indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+from time import sleep
+
+for c in range(10, 0 - 1, -1):
+    sleep(1)
+    print(c)
+print('POW!!!')
+sleep(1)
+print('POW!!!')
+sleep(1)
+print('BOMMMMMM!!!')
