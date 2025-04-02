41daa95b5ff740dd0668631fc07a6170c371e876
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-04-02 14:45:41 -0300

    Create - 047 – Contagem de pares

---
+++ 047 – Contagem de pares.py
@@ -0,0 +1,7 @@
+# Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+from time import sleep
+
+for c in range(2, 51, 2):
+    print(c, end=' ')
+    sleep(0.25)
+print('\nACABOU!!!')
