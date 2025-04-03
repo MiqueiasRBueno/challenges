a0a21b9435e6752450e40eae056cfb23ecc15924
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-04-03 11:49:15 -0300

    Create - 048 – Soma ímpares múltiplos de três

--- 048 – Soma ímpares múltiplos de três.py
+++ 048 – Soma ímpares múltiplos de três.py
@@ -1,8 +1,9 @@
 # Faça um programa que calcule a soma entre todos os números que são múltiplos
 # de três e que se encontram no intervalo de 1 até 500.
+
 soma = cont = 0
 for c in range(1, 500, 2):
     if c % 3 == 0:
         soma += c
         cont += 1
-print(f'A soma de todos os {cont} valores ímpares, no intervalo de 1 à 500 múltiplo de 3 é {soma} ')
+print(f'A soma de todos os {cont} valores ímpares, no intervalo de 1 à 500, múltiplo de 3 é {soma} ')
