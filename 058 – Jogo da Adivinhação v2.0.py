3b31c6e5dfef47e72a981d473e4f24c0b27783f8
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-04-09 10:32:46 -0300

    Create - 058 – Jogo da Adivinhação v2.0

---
+++ 058 – Jogo da Adivinhação v2.0.py
@@ -0,0 +1,22 @@
+# Melhore o jogo do DESAFIO 28 onde o computador vai “pensar” em um número entre 0 e 10.
+# Só que agora o jogador vai tentar adivinhar até acertar,
+# mostrando no final quantos palpites foram necessários para vencer.
+
+from random import randint
+
+computador = randint(0, 10)
+print(f'''\033[1;31m{'=' * 55}\033[m
+\033[1m{'JOGO DE ADIVINHAÇÃO':^55}\033[m
+\033[1;31m{'=' * 55}\033[m
+Sou seu computador, pensei em um número entre 0 e 10.
+Será que você consegue adivinhar?''')
+palpite = int(input('Qual é o seu palpite? '))
+tentativas = 1
+while palpite != computador:
+    if palpite < computador:
+        palpite = int(input('Mais... Tente de novo! '))
+    else:
+        palpite = int(input('Menos ... Tente de novo! '))
+    tentativas += 1
+print(f'''\033[1;31m{'=' * 55}\033[m
+Parabéns, você acertou com {tentativas} tentativas!''')
