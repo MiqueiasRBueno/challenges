6083e6df67da56f577b0f10160361e008154f85a
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-04-10 10:24:13 -0300

    Update - 051 - Progressão Aritmética

--- 051 – Progressão Aritmética.py
+++ 051 – Progressão Aritmética.py
@@ -1,5 +1,6 @@
 # Desenvolva um programa que leia o primeiro termo e a razão de uma PA.
 # No final, mostre os 10 primeiros termos dessa progressão.
+from time import sleep
 
 print(f'''\033[1;31m{'=' * 50}\033[m
 \033[1m{'10 TERMOS DE UMA P.A.':^50}\033[M
@@ -11,6 +12,7 @@ print(f'\033[1;31m{'=' * 50}\033[m')
 # Laço para pa:
 for n in range(1, 11):
     pa = termo + (n -1) * razao
+    sleep(0.5)
     print(pa, end=' ')
 print(f'''\033[1;36mACABOU!!!\033[m
 \033[1;31m{'=' * 50}\033[m''')
