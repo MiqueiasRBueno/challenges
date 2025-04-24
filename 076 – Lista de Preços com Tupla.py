d725632a7ef39cf556d1c2f8f6dd4fa85aa8033d
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-04-24 11:33:28 -0300

    Create - 076 – Lista de Preços com Tupla

---
+++ 076 – Lista de Preços com Tupla.py
@@ -0,0 +1,17 @@
+# Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
+# No final, mostre uma listagem de preços, organizando os dados em forma tabular.
+
+lista = ('Lapís', 1.75,
+         'Caneta', 2.50,
+         'Caderno', 32.00,
+         'Giz de cera', 8.00,
+         'Cola branca', 12.00)
+print(f'''\033[1;31m{'='*40}\033[m
+{'TABELA DE PREÇOS':^40}
+\033[1;31m{'='*40}\033[m''')
+for pos in range(0, len(lista)):
+    if pos % 2 == 0:
+        print(f'{lista[pos]:.<30}', end='')
+    else:
+        print(f'R$ {lista[pos]:>7}')
+print(f'\033[1;31m{'='*40}\033[m')
