bd8b2f131edf9da5b3d5f45ba17b0a20808f960e
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-04-30 16:25:10 -0300

    Create - 082 – Dividindo valores em várias listas

---
+++ 082 – Dividindo valores em várias listas.py
@@ -0,0 +1,22 @@
+# Crie um programa que vai ler vários números e colocar em uma lista.
+# Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados,
+# respectivamente. Ao final, mostre o conteúdo das três listas geradas.
+
+value_list = []
+pares = []
+impares = []
+while True:
+    value_int = int(input('Digite um valor: '))
+    if value_int % 2 == 0:
+        value_list.append(value_int)
+        pares.append(value_int)
+    else:
+        value_list.append(value_int)
+        impares.append(value_int)
+    stop = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
+    if stop in 'N':
+        break
+print(f'''Valores pares digitados são: {pares}
+Valores ímpares digitados são: {impares}
+Todos os valores digitados são: ''', end=' ')
+print(*value_list)
