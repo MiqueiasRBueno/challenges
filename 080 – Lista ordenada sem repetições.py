1b991127e55d45bce391dac6e25a43d830a200d0
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-04-30 09:42:25 -0300

    Create - 080 – Lista ordenada sem repetições

--- 080 – Lista ordenada sem repetições.py
+++ 080 – Lista ordenada sem repetições.py
@@ -1,2 +1,27 @@
 # Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista,
 # já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
+
+print(f'''\033[1;31m{'='*55}\033[m
+{'LISTA ORDENADA SEM REPETIÇÕES':^55}
+\033[1;31m{'='*55}\033[m''')
+value_list = []
+for c in range(0,5):
+    num = int(input(f'Digite o {c+1}º valor: '))
+    if num not in value_list:
+        if c == 0:
+            value_list.append(num)
+            print('Valor adicionado ao final da lista com sucesso!')
+        elif num > value_list[-1]:
+            value_list.append(num)
+            print('Valor adicionado ao final da lista com sucesso!')
+        else:
+            pos = 0
+            while pos < len(value_list):
+                if num <= value_list[pos]:
+                    value_list.insert(pos, num)
+                    print(f'Este valor foi adicionado na {pos} da lista!')
+                    break
+                pos += 1
+print(f'''\033[1;31m{'='*55}\033[m
+{f'Os valores digitados foram: {value_list}':^55}
+\033[1;31m{'='*55}\033[m''')
