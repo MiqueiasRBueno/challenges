894779065373997a5ea3161c5709666f119578c1
miqueias <miqueiasricardobueno@gmail.com>
2025-05-03 20:20:12 -0300

    Create - 084 - Lista Composta e Análise de Dados

--- 084 – Lista composta e análise de dados.py
+++ 084 – Lista composta e análise de dados.py
@@ -2,3 +2,16 @@
 # A) Quantas pessoas foram cadastradas.
 # B) Uma listagem com as pessoas mais pesadas.
 # C) Uma listagem com as pessoas mais leves.
+
+cadastro = []
+while True:
+    nome = str(input('Nome: ')).title().strip()
+    peso = float(input('Peso: '))
+    cadastro.append(nome)
+    cadastro.append(peso)
+    stop = ' '
+    while stop not in 'SN':
+        stop = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
+    if stop == 'N':
+        break
+print(cadastro)
