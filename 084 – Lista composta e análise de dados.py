27452f7fd6818493e8eabe97652ab6a237c9ebb1
miqueias <miqueiasricardobueno@gmail.com>
2025-05-03 21:25:47 -0300

    Create - 084 - Lista Composta e Análise de Dados

--- 084 – Lista composta e análise de dados.py
+++ 084 – Lista composta e análise de dados.py
@@ -3,15 +3,32 @@
 # B) Uma listagem com as pessoas mais pesadas.
 # C) Uma listagem com as pessoas mais leves.
 
-cadastro = []
+cadastro_temp = []
+cadastro_princ = []
+maior = menor = 0
 while True:
-    nome = str(input('Nome: ')).title().strip()
-    peso = float(input('Peso: '))
-    cadastro.append(nome)
-    cadastro.append(peso)
+    cadastro_temp.append(str(input('Nome: ')).title().strip())
+    cadastro_temp.append(float(input('Peso: ')))
+    if len(cadastro_princ) == 0:
+        maior = menor = cadastro_temp[1]
+    else:
+        if cadastro_temp[1] > maior:
+            maior = cadastro_temp[1]
+        if cadastro_temp[1] < menor:
+            menor = cadastro_temp[1]
+    cadastro_princ.append(cadastro_temp[:])
+    cadastro_temp.clear()
     stop = ' '
     while stop not in 'SN':
         stop = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
     if stop == 'N':
         break
-print(cadastro)
+print(f'''Temos o total de {len(cadastro_princ)} pessoas cadastradas!
+O maior peso cadastrado é de {maior:.2f} kg, e pertence a''', end=' ')
+for p in cadastro_princ:
+    if p[1] == maior:
+        print(f'[{p[0]}]', end=' ')
+print(f'\nO menor peso cadastrado é de {menor:.2f} kg e pertence a', end=' ')
+for p in cadastro_princ:
+    if p[1] == menor:
+        print(f'[{p[0]}]', end=' ')
