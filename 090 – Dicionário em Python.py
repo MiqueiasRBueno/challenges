71836675c9b06c33f8714b9f549e9ab03b3f3c8b
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-05-08 23:00:07 -0300

    create - 090 - Dicionário em Python

--- 090 – Dicionário em Python.py
+++ 090 – Dicionário em Python.py
@@ -1,2 +1,16 @@
 # Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
 # No final, mostre o conteúdo da estrutura na tela.
+
+boletim = dict()
+boletim['nome'] = str(input('Nome: ')).title().strip()
+boletim['media'] = float(input(f'Média de {boletim["nome"]}: '))
+if boletim['media'] <= 5:
+    boletim['situacao'] = 'Reprovado'
+elif 7 > boletim['media'] > 5:
+    boletim['situacao'] = 'Recuperação'
+else:
+    boletim['situacao'] = 'Aprovado'
+print(f'\033[1;31m{'=' * 55}\033[m')
+for k, v in boletim.items():
+    print((f'-{k}').title(), (f'é igual a {v}'))
+print(f'\033[1;31m{'=' * 55}\033[m')
