6c41f70e97e89fd22a66b686f24a6ee4dba33c9e
miqueias <miqueiasricardobueno@gmail.com>
2025-05-13 09:21:39 -0300

    Create - 093 – Cadastro de Jogador de Futebol

--- 093 – Cadastro de Jogador de Futebol.py
+++ 093 – Cadastro de Jogador de Futebol.py
@@ -9,13 +9,16 @@ print(f'''\033[1;32m{'-=' * 30}\033[m
 ger_jogadores = dict()
 qtd_gols = list()
 ger_jogadores['Nome'] = str(input('Nome do jogador e ou jogadora: ')).title().strip()
-qtd_partida = int(input(f'Quantas partidas \033[32m{ger_jogadores['Nome']}\033[m jogou? '))
+qtd_partida = int(input(f'Quantas partidas \033[32m{ger_jogadores["Nome"]}\033[m jogou? '))
 print('\033[32m-=\033[m' * 30)
 for q in range(0, qtd_partida):
-    qtd_gols.append(int(input(f'Quantos gols \033[32m{ger_jogadores['Nome']}\033[m'
+    qtd_gols.append(int(input(f'Quantos gols \033[32m{ger_jogadores["Nome"]}\033[m'
                               f' marcou na \033[32m{q + 1}\033[mª partida? ')))
     ger_jogadores['Gols'] = qtd_gols
     ger_jogadores['Total'] = sum(qtd_gols)
+print(f'''\033[32m{'-=' * 30}\033[m
+{ger_jogadores}
+\033[32m{'-=' * 30}\033[m''')
 for k, v in ger_jogadores.items():
     print(f'O valor \033[32m{k}\033[m recebe o valor \033[32m{v}\033[m')
 print(f'''\033[32m{'-=' * 30}\033[m
@@ -25,6 +28,7 @@ tot = 0
 for c in qtd_gols:
     print(f'O jogador marcou \033[32m{c}\033[m gols na \033[32m{1 + tot}\033[mª partida')
     tot += 1
-print(f'O jogador \033[32m{ger_jogadores['Nome']}\033[m marcou uma média '
-      f'de \033[32m{sum(qtd_gols) / qtd_partida:.2f}\033[m gols por partida!')
+print(f'O jogador \033[32m{ger_jogadores["Nome"]}\033[m marcou uma média '
+      f'de \033[32m{sum(qtd_gols) / qtd_partida:.2f}\033[m gols por partida!'
+      f'\n\033[32m{ger_jogadores["Nome"]}\033[m marcou um total de \033[32m{ger_jogadores["Total"]}\033[m gols!')
 print('\033[32m-=\033[m' * 30)
