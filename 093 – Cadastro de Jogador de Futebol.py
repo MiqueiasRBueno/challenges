6df8a9ff7beb1ed8e2a751f6a6470f73a884f26b
MiqueiasRBueno <160036158+MiqueiasRBueno@users.noreply.github.com>
2025-05-12 23:17:26 -0300

    create - 0ex093 – Cadastro de Jogador de Futebol.py

--- 093 – Cadastro de Jogador de Futebol.py
+++ 093 – Cadastro de Jogador de Futebol.py
@@ -2,3 +2,20 @@
 # O programa vai ler o nome do jogador e quantas partidas ele jogou.
 # Depois vai ler a quantidade de gols feitos em cada partida.
 # No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
+
+print(f'''\033[1;32m{'-=' * 30}\033[m
+{'GERENCIAMENTO DE JOGADORES':^60}
+\033[1;32m{'-=' * 30}\033[m''')
+cad_jogador = dict()
+qtd_gols = list()
+cad_jogador['Nome'] = str(input('Nome do Jogador e ou Jogadora: ')).title().strip()
+qtd_partidas = int(input(f'Quantas partidas o jogador e ou jogadora \033[34m{cad_jogador['Nome']}\033[m jogou? '))
+tot = 0
+for qtd in range(0, qtd_partidas):
+    qtd_gols.append(int(input(f'Quantos gols \033[34m{cad_jogador['Nome']}\033[m na \033[34m{1 + tot}\033[mª'
+                              f' partida? ')))
+    cad_jogador['Gols'] = qtd_gols
+    cad_jogador['Total'] = sum(cad_jogador['Gols'])
+    tot += 1
+
+print(cad_jogador)
