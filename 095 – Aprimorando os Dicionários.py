136fa23dfd9c99d2c8e252313490fbe735f99139
miqueias <miqueiasricardobueno@gmail.com>
2025-05-21 13:39:53 -0300

    Create - 095 – Aprimorando os Dicionários

--- 095 – Aprimorando os Dicionários.py
+++ 095 – Aprimorando os Dicionários.py
@@ -1,10 +1,11 @@
 # Aprimore o desafio 93 para que ele funcione com vários jogadores,
 # incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
 
-
 jogadores_dicio = dict()
 jogadores_lista = list()
 num_gols = []
+def linhas():
+    print(f'\033[1;32m{"-=" * 27}\033[m')
 while True:
     jogadores_dicio['Nomes'] = str(input('Nomes do jogador e ou jogadora: ')).title().strip()
     num_partidas = int(input(f'Quantas partidas o jogador e ou jogadora {jogadores_dicio["Nomes"]} jogou?: '))
@@ -22,11 +23,11 @@ while True:
         print('ERRO! Digite S ou N/: ')
     if stop in 'N':
         break
-print(f'\033[1;32m{"-=" * 27}\033[m')
+linhas()
 print(f'{"_CÓD":<10}{"NOME":^8}{"GOLS":^15}{"TOTAL":>20}')
 for i, j in enumerate(jogadores_lista):
     print(f' {i:<5}{j["Nomes"]:^15}{f'{j["Gols"]}':^13}{j["Total"]:^35}')
-print('\033[1;32m-=\033[m' * 27)
+linhas()
 while True:
     mostrar = int(input('Mostrar dados de qual jogador (999 Para parar): '))
     if mostrar == 999:
@@ -34,10 +35,11 @@ while True:
     if mostrar >= len(jogadores_lista):
         print(f'ERRO! Não existe jogador e ou jogadora com o código {mostrar}!')
     else:
+        linhas()
         print(f' __LEVANTAMENTO DO JOGADOR E OU JOGADORA {jogadores_lista[mostrar]["Nomes"]}:')
         partida = 0
         for g in jogadores_lista[mostrar]['Gols']:
             print(f'No {1 + partida}ª jogo ele fez {g} gols')
             partida += 1
-    print('\033[1;32m-=\033[m' * 27)
+    linhas()
 print(f'\n{'\033[1;32m<<\033[mVolte Sempre!\033[1;32m>>\033[m':^70}')
