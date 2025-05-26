edd4426bcf44d8c599d1ecd741971a19db8f6dad
miqueias <miqueiasricardobueno@gmail.com>
2025-05-26 17:30:56 -0300

    Create - 098 – Função de Contador

--- 098 – Função de Contador.py
+++ 098 – Função de Contador.py
@@ -7,38 +7,34 @@ from time import sleep
 
 
 # Configuração da função contador:
-def contador():
-    print('\033[1;32m=\033[m' * 55)
-    sleep(0.5)
-    print('a) Contagem de 1 até 10, de 1 em 1:')
-    sleep(0.5)
-    for n in range(1, 11, 1):
-        sleep(0.5)
-        print(n, end=' ')
-    sleep(0.5)
-    print('FIM')
-    print('\033[1;32m=\033[m' * 55)
-    sleep(0.5)
-    print('b) Contagem de 10 até 0, de 2 em 2:')
-    sleep(0.5)
-    for n1 in range(10, 0, -2):
-        sleep(0.5)
-        print(n1, end=' ')
-    sleep(0.5)
-    print('FIM')
-    print('\033[1;32m=\033[m' * 55)
-    sleep(0.5)
-    print('Agora é sua vez de personalizar a contagem:')
-    sleep(0.5)
-    inicio = int(input('Início: '))
-    fim = int(input('Fim: '))
-    passo = int(input('Passo: '))
-    for i in range(inicio, fim, passo):
-        sleep(0.5)
-        print(i, end=' ')
-        sleep(0.5)
-    print('FIM')
+def contador(i, f, p):
+    if p < 0:
+        p *= - 1
+    if p == 0:
+        p = 1
 
+    print('\033[31m=\033[m' * 40)
+    print(f'Contagem de {i} até {f}, de {p} em {p}:')
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont}', end=' ')
+            sleep(0.5)
+            cont += p
+        print('Fim!')
+    else:
+        cont = i
+        while cont >= 1:
+            print(f'{cont}', end=' ')
+            sleep(0.5)
+            cont -= p
+        print('Fim')
 
 # Programa principal:
-contador()
+contador(1, 10, 1)
+contador(10, 0, 2)
+print('\033[31m=\033[m' * 40)
+ini = int(input('Início: '))
+fim = int(input('Fim: '))
+pas = int(input('Passo: '))
+contador(ini, fim, pas)
