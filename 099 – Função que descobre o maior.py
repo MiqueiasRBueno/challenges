f70849585e6242d54ccacf85c3f41dccc5fb2ec1
miqueias <miqueiasricardobueno@gmail.com>
2025-05-27 08:52:52 -0300

    Create - 099 – Função que descobre o maior

--- 099 – Função que descobre o maior.py
+++ 099 – Função que descobre o maior.py
@@ -1,2 +1,18 @@
 # Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros.
 # Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+
+def maior(*num):
+    print('\033[1;31m=\033[m' * 45)
+    print('Analisando os valores passados...')
+    tot = 0
+    for c in num:
+        print(c, end=' ')
+        tot += 1
+    print(f' _Foram informados {tot} valores!')
+    print(f'O maior valor informado foi {max(num)}')
+
+
+# Programa Principal:
+maior(1, 3, 6, 7, 9)
+maior(4, 5 , 2, 7, 8, 9, 3, 90, 2132)
+maior(0)
