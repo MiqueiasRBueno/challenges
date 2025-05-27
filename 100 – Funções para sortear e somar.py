36e38cad71bb0954c3017f3d5de32f8c5acc861d
miqueias <miqueiasricardobueno@gmail.com>
2025-05-27 17:05:52 -0300

    Create - 100 – Funções para sortear e somar

--- 100 – Funções para sortear e somar.py
+++ 100 – Funções para sortear e somar.py
@@ -3,23 +3,28 @@
 # todos os valores pares sorteados pela função anterior.
 
 from random import randint
+from time import sleep
 
 numeros = list()
 # função para sortear 5 valores:
 def sorteia(lista):
-    print('\033[32m-\033[m' * 45)
+    print('\033[32m-\033[m' * 53)
     print('Sorteando 5 valores: ', end=' ')
+    for t in range(0, 3):
+        print('.', end=' ')
+        sleep(0.3)
     for n in range(0, 5):
-        num = randint(0, 10)
+        num = randint(1, 10)
         lista.append(num)
     for valor in lista:
         print(valor, end=' ')
+        sleep(0.3)
     print()
 
 # função para a soma de pares:
 def somapar(lista):
-    print('\033[32m-\033[m' * 45)
-    print(f'A soma dos valores pares são ', end='')
+    print('\033[32m-\033[m' * 53)
+    print(f'A soma dos valores pares de {lista}, temos: ', end='')
     soma = 0
     for valor in lista:
         if valor % 2 == 0:
@@ -29,4 +34,5 @@ def somapar(lista):
 
 # Programa principal
 sorteia(numeros)
+sleep(0.3)
 somapar(numeros)
