750f04b17a259e11031abfe6a6307d65a03583e6
miqueias <miqueiasricardobueno@gmail.com>
2025-06-11 16:54:42 -0300

    Create - 109 – Formatando Moedas em Python

--- 109 – Formatando Moedas em Python/moeda.py
+++ 109 – Formatando Moedas em Python/moeda.py
@@ -1,46 +1,50 @@
-def aumentar(preco=0, taxa=0):
+def aumentar(preco=0, taxa=0, formato=False):
     """
     _> Função para aumentar uma porcentagem de um valor digitado pelo usuário.
     :param preco: Número a ser aumentado
     :param taxa: valor da porcentagem a ser aumentada
+    :param formato: Função para formatação de moeda
     :return: retorna novo valor com o aumento
     """
     res = preco + (preco * taxa/100)
-    return res
+    return res if not formato else moeda(res)
 
 
-def diminuir(preco=0, taxa=0):
+def diminuir(preco=0, taxa=0, formato=False):
     """
     _> Função que diminui uma porcentagem de um valor digitado pelo usuário.
     :param preco: Número a ser diminuído
     :param taxa: valor da porcentagem a ser diminuída
+    :param formato: Função para formatação de moeda
     :return: retorna o novo valor diminuído a porcentagem digitada
     """
     res = preco - (preco * taxa/100)
-    return res
+    return res if not formato else moeda(res)
 
 
-def dobro(preco=0):
+def dobro(preco=0, formato=False):
     """
     _> Dobra um valor digitado pelo usuário
     :param preco: valor digitado pelo usuário para ser dobrado
+    :param formato: Função para formatação de moeda
     :return: retorna o dobro do valor digitado
     """
     res = preco * 2
-    return res
+    return res if not formato else moeda(res)
 
 
-def metade(preco=0):
+def metade(preco=0, formato=False):
     """
     _> Função que mostra a metade do valor digitado pelo usuário
     :param preco: valor digitado pelo usuário para ser dividido pela metade
+    :param formato: Função para formatação de moeda
     :return: retorna a metade do valor digitado
     """
     res = preco / 2
-    return res
+    return res if not formato else moeda(res)
 
 
-def moeda(preco=0, tmoeda='R$'):
+def moeda(preco, tmoeda='R$'):
     """
     _> Função para formatação monetária
     :param preco: Valor a ser formatado
