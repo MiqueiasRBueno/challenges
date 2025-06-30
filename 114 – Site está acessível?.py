c1f9da5bbe8c52ba5c32650d47ee38fff616038b
miqueias <miqueiasricardobueno@gmail.com>
2025-06-30 16:44:14 -0300

    Create - 114 – Site está acessível?

--- 114 – Site está acessível?.py
+++ 114 – Site está acessível?.py
@@ -2,15 +2,13 @@
 
 import requests
 
-def verificar_pudim():
-    url = "http://www.pudim.com.br"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        print(f'O site {url} está acessível"')
-    except requests.exceptions.RequestException as e:
-        print(f'O site {url} não está acessível: \n{e}')
+try:
+    url = "https://www.pudim.com.br"
+    response = requests.get(url)
+    response.raise_for_status()
+    print(f'O site Pudim está acessível"')
+except requests.exceptions.RequestException as e:
+    print(f'O site Pudim não está acessível: \n{e}')
 
 
-if __name__ == '__main__':
-    verificar_pudim()
+
